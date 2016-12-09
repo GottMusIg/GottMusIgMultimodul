0e37d8e47694f93248261e52fc0929e8fe5f909e
Leonn1997 <leon.gottschick@web.de>
2016-12-09 13:44:20 +0100

    renamed Location to RealmLocation

---
+++ src/main/java/com/gottmusig/searchcharacter/utils/RealmLocation.java
@@ -0,0 +1,27 @@
+package com.gottmusig.searchcharacter.utils;
+
+import java.util.Arrays;
+import java.util.List;
+/**
+ * @author lgottschick
+ * @since 27.09.2016
+ */
+public enum RealmLocation {
+    de_DE,
+    en_GB,
+    ru_RU,
+    fr_FR,
+    es_ES,
+    it_IT,
+    pt_BR;
+
+
+	/**
+	 * Creates a list with all {@link RealmLocation} and returns it.
+	 * 
+	 * @return A list with all {@link RealmLocation}
+	 */
+    public static List<RealmLocation> getLocations() {
+		return Arrays.asList(RealmLocation.values());
+	}
+}
