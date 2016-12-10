3c58f5011ef9f27fa3516e96de129a72d59b8262
Leonn1997 <leon.gottschick@web.de>
2016-12-10 18:34:17 +0100

    removed EntityListener

--- src/main/java/com/gottmusig/searchcharacter/jpa/RealmLocationEntity.java
+++ src/main/java/com/gottmusig/searchcharacter/jpa/RealmLocationEntity.java
@@ -1,15 +1,11 @@
 package com.gottmusig.searchcharacter.jpa;
 
 import com.gottmusig.searchcharacter.domain.api.RealmLocation;
-import com.gottmusig.utils.SpringEntityListener;
-
-import javax.persistence.EntityListeners;
 
 /**
  * @author lgottschick
  * @since 27.09.2016
  */
-@EntityListeners(SpringEntityListener.class)
 public class RealmLocationEntity implements RealmLocation {
 
     private Location location;
