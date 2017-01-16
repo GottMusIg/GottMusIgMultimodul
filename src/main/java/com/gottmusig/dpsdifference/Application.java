01b039a54f8f212bbe32c1224e82d912547e0b2c
Leonn1997 <leon.gottschick@web.de>
2017-01-16 22:23:19 +0100

    refactoring

--- src/main/java/com/gottmusig/dpsdifference/Application.java
+++ src/main/java/com/gottmusig/dpsdifference/Application.java
@@ -1,13 +1,5 @@
 package com.gottmusig.dpsdifference;
 
-import com.gottmusig.searchcharacter.configuration.SearchCharacterConfiguration;
-import com.gottmusig.searchcharacter.domain.api.Realm;
-import com.gottmusig.searchcharacter.domain.api.SearchCharacter;
-import com.gottmusig.searchcharacter.jpa.Location;
-import com.gottmusig.searchcharacter.jpa.RealmEntity;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 /**
  * @author leong
  * @since 24.11.2016
@@ -17,7 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-
+        /*
         ApplicationContext context = new AnnotationConfigApplicationContext(SearchCharacterConfiguration.class);
         SearchCharacter searchCharacter = context.getBean(SearchCharacter.class);
         RealmEntity realmEntity = new RealmEntity();
@@ -27,6 +19,7 @@ public class Application {
         Realm realm = searchCharacter.getAllRealms().get(0);
         System.out.println(realm.getLocation());
         System.out.println(realm.getName());
+        */
     }
 
 }
