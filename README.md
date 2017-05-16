7f04f530e9f654785c0e880c7205e0b76f6fd567
Leonn1997 <leon.gottschick@web.de>
2017-05-16 10:06:48 +0200

    GOMU-46 - Return items for a requested character

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DatabaseService
+
+[![Coverage Status](https://coveralls.io/repos/github/GottMusIg/DatabaseService/badge.svg?branch=master)](https://coveralls.io/github/GottMusIg/DatabaseService?branch=master)
--- src/main/java/com/gottmusig/database/service/domain/item/Item.java
+++ src/main/java/com/gottmusig/database/service/domain/item/Item.java
@@ -1,9 +1,11 @@
 package com.gottmusig.database.service.domain.item;
 
+import com.gottmusig.database.service.domain.Entity;
+
 /**
  * Created by leong on 03.04.2017.
  */
-public interface Item {
+public interface Item extends Entity {
 
     String getWowHeadTooltip();
 
