b258ceea77a39b9f744fafda2c7d161734258c54
Leonn1997 <leon.gottschick@web.de>
2017-05-16 14:18:54 +0200

    GOMU-62 - Junit-Tests for databasemodel

--- src/test/java/com/gottmusig/database/service/domain/character/jpa/CharacterServiceImplTest.java
+++ src/test/java/com/gottmusig/database/service/domain/character/jpa/CharacterServiceImplTest.java
@@ -54,4 +54,5 @@ public class CharacterServiceImplTest {
         assertEquals("Warrior", wowClassEntity.getName());
     }
 
+
 }
--- src/test/java/com/gottmusig/database/service/domain/dpsdifference/jpa/SpecificationDPSEntityTest.java
+++
@@ -1,9 +0,0 @@
-package com.gottmusig.database.service.domain.dpsdifference.jpa;
-
-/**
- * @author leong
- * @since 11.05.2017
- */
-public class SpecificationDPSEntityTest {
-
-}
