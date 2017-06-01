82e7cbcdb423cd6dd898b53597e48384d3637fc2
cemig <cemig@united-internet.de>
2017-06-01 08:41:28 +0200

    First try of implementing simulation in middleware

--- database-service/src/main/java/com/gottmusig/database/service/domain/simulation/jpa/SimulationServiceImpl.java
+++ database-service/src/main/java/com/gottmusig/database/service/domain/simulation/jpa/SimulationServiceImpl.java
@@ -1,7 +1,12 @@
 package com.gottmusig.database.service.domain.simulation.jpa;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import com.gottmusig.database.service.domain.character.Character;
 import com.gottmusig.database.service.domain.simulation.SimulationService;
+import com.gottmusig.gottmusig.model.dpscalculation.SimulationCraft;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,8 +18,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class SimulationServiceImpl implements SimulationService {
 
+    private static final String BASE_URL = "http://localhost:8080/"; //TODO
+    private static final String SIMULATION_PATH = "gottmusig-simulation/rest/simulation/player";
+
+    private static final String REGION = "region";
+    private static final String REALM = "realm";
+    private static final String NAME = "name";
+
+
     @Override
     public Character simulateDPS(Character character) {
-        return null;
+
+        Client c = ClientBuilder.newClient();
+        WebTarget target = c.target(BASE_URL).path(SIMULATION_PATH) //
+             .queryParam(REGION, "eu") //TODO
+        .queryParam(REALM, character.getRealm()) //
+        .queryParam(NAME, character.getName());
+
+
+        SimulationCraft simulation = target.request().get(SimulationCraft.class);
+        character.setDPS(simulation.getSim().getPlayers().get(0).getCollectedData().getDps().getMean().intValue());
+        return character;
+
     }
 }
